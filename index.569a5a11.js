86126f366ce4bf8eeecc88dca52dba13c433bae2
supermacchan <aiba.aki9@gmail.com>
2023-05-26 14:30:05 +0000

    Deploying to gh-pages from @ supermacchan/digital-product-agency-landing@ba012cc984a59e9554d0be1834194fdc941de56f 🚀

--- index.569a5a11.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".reviews__slider").slick({dots:!0,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1,vertical:!0,autoplay:!0})})),document.querySelector(".up").addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
-//# sourceMappingURL=index.569a5a11.js.map
