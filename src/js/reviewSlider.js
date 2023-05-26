ea843c4871012d77645431c6c0457f6c1130ce75
Olga Birkovska <aiba.aki9@gmail.com>
2023-05-26 16:06:47 +0300

    reviews slider

---
+++ src/js/reviewSlider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".reviews__slider").slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        // adaptiveHeight: true
+    });
+});
